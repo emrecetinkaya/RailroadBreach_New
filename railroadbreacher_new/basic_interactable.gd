0bae924fcc77b20f0948dd28aaee23be7231585d
baychowski <117860863+baychowski@users.noreply.github.com>
2024-10-18 03:20:56 +0300

    fırlatıyorum

--- railroadbreacher_new/basic_interactable.gd
+++ railroadbreacher_new/basic_interactable.gd
@@ -4,12 +4,15 @@ extends RigidBody3D
 @onready var new_parent = $"../Character/Marker3D"
 @onready var grabbed = false
 
+func _physics_process(delta: float) -> void:
+	pass
 
 func grab():
 	if !grabbed:
 		freeze = true
-		self.reparent(new_parent, false)
-		global_transform.origin = new_parent.transform.origin
+		self.reparent(new_parent, true)
+		if grabbed:
+			global_transform.origin = new_parent.transform.origin
 		collision_layer = 0
 		collision_mask = 0
 		grabbed = true
@@ -18,7 +21,7 @@ func grab():
 func secondary():
 	if grabbed:
 		freeze = false
-		self.reparent(original_parent, false)
+		self.reparent(original_parent, true)
 		collision_layer = 1
 		collision_mask = 1
 		grabbed = false
