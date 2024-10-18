dc56e50c438fdb7d71a1a639f0a9d3d8fb1e7524
baychowski <117860863+baychowski@users.noreply.github.com>
2024-10-18 20:07:52 +0300

    vololo

--- railroadbreacher_new/control.gd
+++ railroadbreacher_new/control.gd
@@ -1,6 +1,8 @@
 extends Control
 
 
+func _ready() -> void:
+	$ProgressBar.value = randi_range(32400,64800)
 func _on_button_pressed() -> void:
 	$LineEdit.insert_text_at_caret("1")
 
@@ -50,5 +52,26 @@ func _on_button_11_pressed() -> void:
 
 
 func _on_button_12_pressed() -> void:
-	print($LineEdit.text)
+	var submit = $LineEdit.text 
 	$LineEdit.clear()
+
+func _physics_process(delta: float) -> void:
+	$ProgressBar.value -= 1
+	print($ProgressBar.value)
+	if submit >=17 and $ProgressBar.value >= 61200:
+		print("correct")
+	if (submit >= 16 and submit < 17) and ($ProgressBar.value >= 57600 and $ProgressBar.value < 61200):
+		print("correct")
+	if (submit >=15 and submit < 16) and ($ProgressBar.value >= 54000 and $ProgressBar.value < 57600):
+		print("correct")
+	if (submit >= 14 and submit < 15) and ($ProgressBar.value >= 50400 and $ProgressBar.value < 54000):
+		print("correct")
+	if (submit >= 13 and submit < 14) and ($ProgressBar.value >= 46800 and $ProgressBar.value < 50400):
+		print("correct")
+	if (submit >= 12 and submit < 13) and ($ProgressBar.value >= 43200 and $ProgressBar.value < 46800):
+		print("correct")
+	if (submit >= 11 and submit < 12) and ($ProgressBar.value >= 39600 and $ProgressBar.value < 43200):
+		print("correct")
+	if (submit >= 10 and submit < 11) and ($ProgressBar.value >= 36000 and $ProgressBar.value < 39600):
+		print("correct")
+		
