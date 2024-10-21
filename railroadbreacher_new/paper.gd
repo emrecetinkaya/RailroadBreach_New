6048370e9508bbc54c90cc6a6174d454cf585eda
baychowski <117860863+baychowski@users.noreply.github.com>
2024-10-21 16:29:24 +0300

    Smooth Operator Crouchin'

--- railroadbreacher_new/paper.gd
+++ railroadbreacher_new/paper.gd
@@ -1,10 +1,11 @@
 extends RigidBody3D
 @onready var original_parent = get_parent()
 @onready var original_origin = transform.origin
-@onready var new_parent = $SubViewportContainer/SubViewport/Character/Marker3D
-@onready var character = $SubViewportContainer/SubViewport/Character
+@onready var new_parent = $"../SubViewportContainer/SubViewport/Character/Marker3D"
+@onready var character = $"../SubViewportContainer/SubViewport/Character"
 @onready var grabbed = false
-@onready var target = $SubViewportContainer/SubViewport/Character/Marker3D2
+@onready var target = $"../SubViewportContainer/SubViewport/Character/Marker3D2"
+
 
 
 
@@ -14,7 +15,6 @@ extends RigidBody3D
 func _physics_process(delta: float) -> void:
 	if grabbed: 
 		look_at(new_parent.global_position)
-		print(position)
 
 func grab():
 	if !grabbed:
